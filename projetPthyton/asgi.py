b538e801d4cf2d9343a20d5607c97fe1ebc93fac
EmileNapon <emilenapon0@gmail.com>
2025-04-07 20:56:03 +0000

    first commit

---
+++ projetPthyton/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for projetPthyton project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projetPthyton.settings')
+
+application = get_asgi_application()
